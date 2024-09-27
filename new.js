e22a1e2ff1f8d9196a8f7d81a0b7faea6142a1f0
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-09-27 23:57:47 +0300

    added content to the shop page

---
+++ new.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelector(".show-modal");
+
+for (let i = 0; i < btnOpenModal; i++)
+  btnOpenModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
