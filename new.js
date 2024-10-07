302e32b7b545ff2278646e5a3986065e1abd032e
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-10-08 00:01:43 +0300

    rock paper sciscors game

--- new.js
+++ new.js
@@ -11,3 +11,16 @@ for (let i = 0; i < btnOpenModal; i++)
     overlay.classList.remove("hidden");
   });
 
+function computerMoves(){
+  const randomNumber = Math.random();
+  let computerMove = " ";
+
+  if(randomNumber >= 0 && randomNumber < 1/3){
+    computerMove = "rock";
+  }else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove = "paper";
+  }else (randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = "sciscors"
+  }
+  return computerMove;
+}
