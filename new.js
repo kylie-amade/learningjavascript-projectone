5028dd84027aebdf283dda41c5a1c416df7c61cb
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-10-22 00:10:13 +0300

    update new.js

--- new.js
+++ new.js
@@ -11,16 +11,64 @@ for (let i = 0; i < btnOpenModal; i++)
     overlay.classList.remove("hidden");
   });
 
-function computerMoves(){
+
+//Rock,paper,scissors
+const score = {
+}
+document.querySelector(".rock").addEventListener("click" ,function(){
+  playGame("rock");
+} ) ;
+document.querySelector(".paper").addEventListener("click", function(){
+  playGame("paper");
+} ) ;
+document.querySelector(".scissors").addEventListener("click" , function(){
+  playGame("paper");
+} ) ;
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMoves();
+ 
+
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      alert("You Lose");
+    } else if (computerMove === "paper") {
+      alert("You Win");
+    } else if (computerMove === "scissors") {
+      alert("Tie");
+    }
+  } else if (playerMove === "paper") {
+   if (computerMove === "rock") {
+     alert("You Lose");
+   } else if (computerMove === "paper") {
+     alert("You Win");
+   } else if (computerMove === "scissors") {
+     alert("Tie");
+   }
+  } else if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      alert("You Lose");
+    } else if (computerMove === "paper") {
+      alert("You Win");
+    } else if (computerMove === "scissors") {
+      alert("Tie");
+    }
+  }
+  
+  
+}
+
+function pickComputerMoves() {
   const randomNumber = Math.random();
   let computerMove = " ";
-
-  if(randomNumber >= 0 && randomNumber < 1/3){
+ 
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = "rock";
-  }else if(randomNumber >= 1/3 && randomNumber < 2/3){
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
     computerMove = "paper";
-  }else (randomNumber >= 2/3 && randomNumber < 1){
-    computerMove = "sciscors"
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+  computerMove = "scissors";
   }
   return computerMove;
+   
 }
